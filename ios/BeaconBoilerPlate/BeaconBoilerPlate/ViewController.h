4f99dbb72a9cdf4c017f707cf11fa34167643b02
Sarra Srairi <ssr@Mac-mini.local>
2016-03-21 14:40:40 +0100

    #create iOS repo

---
+++ ios/BeaconBoilerPlate/BeaconBoilerPlate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconBoilerPlate
+//
+//  Created by sarra srairi on 21/03/2016.
+//  Copyright © 2016 sarra srairi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
