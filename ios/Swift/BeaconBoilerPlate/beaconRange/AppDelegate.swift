839d938037f2ee746dd0979f4178798faafd7880
Sarra Srairi <ssr@Mac-mini.local>
2016-09-19 13:19:55 +0200

    #Update swift code with the new Swift version #Still working with localnotification

--- ios/Swift/BeaconBoilerPlate/beaconRange/AppDelegate.swift
+++ ios/Swift/BeaconBoilerPlate/beaconRange/AppDelegate.swift
@@ -10,6 +10,9 @@ import UIKit
 import ATAnalytics
 import ATLocationBeacon
 import AVFoundation
+import UserNotifications
+
+
 
 @UIApplicationMain
 class AppDelegate: ATBeaconAppDelegate, UIApplicationDelegate,ATBeaconNotificationDelegate {
@@ -18,57 +21,73 @@ class AppDelegate: ATBeaconAppDelegate, UIApplicationDelegate,ATBeaconNotificati
     
     
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // HELP:
-        // init the adtag platforme with the
-        // ** user Login : Login delivred by the Connecthings staff
-        // ** user Password : Password delivred by the Connecthings staff
-        // ** user Compagny : ....
-        // ** beaconUuid : - UUID beacon number devivred by the Connecthings staff
         
-        initAdtagInstanceWithUrlType(ATUrlTypeProd ,userLogin: "_LOGIN_" ,userPassword: "_PASSWORD_" ,userCompany: "_COMPANY_" ,beaconUuid: "___UUID___");
         
-        if  ((launchOptions?[UIApplicationLaunchOptionsLocationKey] as? NSDictionary) != nil) {
+        /*
+         // Override point for customization after application launch.
+         // HELP:
+         // init the adtag platforme with the
+         // ** user Login : Login delivred by the Connecthings staff
+         // ** user Password : Password delivred by the Connecthings staff
+         // ** user Compagny : Define the compagny name
+         // ** beaconUuid : - UUID beacon number devivred by the Connecthings staff
+         */
+        
+        initAdtagInstance(with: ATUrlTypePreprod, userLogin: "**LOGIN**", userPassword: "**PASSWORD**", userCompany: "**COMPAGNY**", beaconUuid: "**UUID**")
+        
+        if #available(iOS 10.0, *) {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                // Enable or disable features based on authorization.
+            }
+        } else {
+            if(UIApplication.instancesRespond(to: #selector(UIApplication.registerUserNotificationSettings(_:)))){
+                let notificationCategory:UIMutableUserNotificationCategory = UIMutableUserNotificationCategory()
+                notificationCategory.identifier = "INVITE_CATEGORY"
+                //registerting for the notification.
+                UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings (types: [.alert, .badge, .sound], categories: nil))
+            }
+            
         }
         
-        if(UIApplication.instancesRespondToSelector(#selector(UIApplication.registerUserNotificationSettings(_:)))){
-            let notificationCategory:UIMutableUserNotificationCategory = UIMutableUserNotificationCategory()
-            notificationCategory.identifier = "INVITE_CATEGORY"
-            //registerting for the notification.
-            application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes:[ .Sound, .Alert,
-                .Badge], categories: nil));
-        }
         return true
     }
     
-    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
-        
-        
+    
+    override func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        super.applicationWillResignActive(application)
     }
     
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
     
-    override func applicationDidBecomeActive(application: UIApplication) {
-        super.applicationDidBecomeActive(application)
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        super.applicationDidBecomeActive(application);
+        
     }
     
-    override func applicationWillResignActive (application: UIApplication){
-        super.applicationWillResignActive(application)
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
     }
+    
     
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    func createNotification(_beaconContent: ATBeaconContent!) -> UILocalNotification! {
+    
+    func createNotification(_ _beaconContent: ATBeaconContent!) -> UILocalNotification! {
         let kLocalNotificationMessage:String! = _beaconContent.getNotificationDescription()
         let kLocalNotificationAction:String! = _beaconContent.getAlertTitle()
         let localNotification:UILocalNotification = UILocalNotification()
@@ -76,7 +95,7 @@ class AppDelegate: ATBeaconAppDelegate, UIApplicationDelegate,ATBeaconNotificati
         localNotification.alertAction = kLocalNotificationAction
         print("create notification from app delegate");
         localNotification.soundName = UILocalNotificationDefaultSoundName
-        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        UIApplication.shared.scheduleLocalNotification(localNotification)
         
         return localNotification;
     }
--- ios/Swift/BeaconBoilerPlate/beaconRange/ViewController.swift
+++ ios/Swift/BeaconBoilerPlate/beaconRange/ViewController.swift
@@ -11,6 +11,37 @@ import ATAnalytics
 import ATLocationBeacon
 
 class ViewController: UIViewController,ATRangeDelegate {
+    /**
+     *  this delegate will be called when a beacon start ranging the didRangeBeacons method. it  permits get status about the BeaconContent loading from the adtag platform.   *
+     *  @param _beacons             the beacon ranging around the region.
+     *  @param _beaconContents      the beacon's information.
+     *  @param informationStatus    indicate if the SDK has loaded all the information about the beacons around from the adtag platform.
+     *  @param feedstatus           gives information about the connection status.
+     *  @param region               specific region when beacon are ranging around.
+     */
+    @available(iOS 4.0, *)
+    public func didRangeBeacons(_ _beacons: [Any]!, beaconContents: [Any]!, informationStatus: ATRangeInformationStatus, feedStatus: ATRangeFeedStatus, region: CLRegion!) {
+        var feedStatusString: String
+        
+        
+        switch feedStatus {
+        case ATRangeFeedStatusInProgress:
+            feedStatusString = "IN_PROGRESS"
+        case ATRangeFeedStatusBackendError:
+            feedStatusString = "BACKEND_ERROR"
+        case ATRangeFeedStatusNetworkError:
+            feedStatusString = "NETWORK_ERROR"
+        case ATRangeFeedStatusBackendSuccess:
+            feedStatusString = "BACKEND_SUCCESS"
+        default:
+            feedStatusString = ""
+        }
+        
+        
+        self.txt_nbrBeacon.text = String(format: NSLocalizedString("beaconDetected", comment: ""), feedStatusString, _beacons.count, beaconContents.count)
+
+    }
+
     
     @IBOutlet weak var txt_nbrBeacon: UILabel!
      var beaconManager: ATBeaconManager!
@@ -26,31 +57,6 @@ class ViewController: UIViewController,ATRangeDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
- func didRangeBeacons(beacons: [AnyObject], beaconContents: [AnyObject], informationStatus: ATRangeInformationStatus, feedStatus: ATRangeFeedStatus, region: CLRegion!) {
-        var feedStatusString: String
-        
-        
-        switch feedStatus {
-        case ATRangeFeedStatusInProgress:
-            feedStatusString = "IN_PROGRESS"
-        case ATRangeFeedStatusBackendError:
-            feedStatusString = "BACKEND_ERROR"
-        case ATRangeFeedStatusNetworkError:
-            feedStatusString = "NETWORK_ERROR"
-        case ATRangeFeedStatusBackendSuccess:
-            feedStatusString = "BACKEND_SUCCESS"
-            default:
-             feedStatusString = ""
-        }
-     
-        
-        self.txt_nbrBeacon.text = String(format: NSLocalizedString("beaconDetected", comment: ""), feedStatusString, beacons.count, beaconContents.count)
-
-    
-    }
-
- 
+    } 
 }
 
